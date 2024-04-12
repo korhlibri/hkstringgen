0d8a45eeb2265a4b9c0d2498b107aa0a5dd3e625
hk122001 <hlibk@proton.me>
2024-04-12 17:09:55 -0600

    Comment code, finalize details

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ use rand_core::{RngCore, OsRng};
 use std::{str, process, thread, time};
 use mouse_position::mouse_position::Mouse;
 
+/// Program to generate truly random strings
 #[derive(Parser)]
 struct Args{
+    /// Range of the string to generate (from 0 to 255)
     #[arg(short, long, default_value_t = 8)]
     range: u8,
+    /// Include numbers in string
     #[arg(short, long, action)]
     numbers: bool,
+    /// Include lowercase letters in string 
     #[arg(short, long, action)]
     lowercase: bool,
+    /// Include uppercase letters in string
     #[arg(short, long, action)]
     uppercase: bool,
+    /// Include special characters in string
     #[arg(short, long, action)]
     special: bool,
+    /// Include mouse movement in randomization (Significant mouse movement recommended)
     #[arg(short, long, action)]
     mouse: bool,
 }
@@ -27,15 +34,18 @@ fn main() {
         process::exit(1);
     }
 
-    let mut seconds_for_mouse: u8 = 5;
+    let mut seconds_for_mouse: u8 = 10;
 
+    // Gets randomness from system (supposed to be cryptographically safe)
     let mut result = vec![0u8; args.range.into()];
     match OsRng.try_fill_bytes(&mut result) {
         Ok(b) => b,
-        Err(_) => seconds_for_mouse = 10,
+        Err(_) => seconds_for_mouse = 20,
     };
 
-    if !args.mouse && seconds_for_mouse == 10{
+    // If failing to get randomness from system, force the user to use mouse coordinates.
+    // Also doubles the seconds required to generate randomness.
+    if !args.mouse && seconds_for_mouse == 20{
         eprintln!("Error: Failed to get randomness from system. In order to generate random values, mouse movement needs to be recorded. Use --mouse to record mouse movement.");
         process::exit(1);
     } else if args.mouse {
@@ -44,6 +54,7 @@ fn main() {
 
     bytes_to_utfchars(&mut result, args.numbers, args.lowercase, args.uppercase, args.special);
 
+    // Converts utf8 bytes to readable characters
     let result_str = match str::from_utf8(&result) {
         Ok(s) => s,
         Err(e) => {
@@ -61,6 +72,7 @@ fn mouse_coords_to_random(result: &mut Vec<u8>, seconds: u8, vecrange: u8){
     let mut lastx: i32 = 0;
     let mut lasty: i32 = 0;
     let mut i: u8 = 0;
+
     while curriter <= iter {
         let position = Mouse::get_mouse_position();
         let (x, y) = match position {
@@ -70,13 +82,19 @@ fn mouse_coords_to_random(result: &mut Vec<u8>, seconds: u8, vecrange: u8){
                 process::exit(1);
             },
         };
+
+        // Makes sure that the coordinates are always different
         if x == lastx && y == lasty {
+            thread::sleep(time::Duration::from_millis(10));
             continue;
         }
+
         lastx = x;
         lasty = y;
         let pos = x + y;
+
         result[i as usize] = result[i as usize].wrapping_add((pos % 256) as u8);
+
         i = (i + 1) % vecrange;
         curriter += 1;
         thread::sleep(time::Duration::from_millis(5));
@@ -89,7 +107,9 @@ fn bytes_to_utfchars(result: &mut Vec<u8>, numop: bool, lowop: bool, uppop: bool
     const UPPERLETTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const SPECIALCHARS: &str = " -~!@#$%^&*_+=`|(){}[:;\"'<>,.?]";
 
+    // Range of the included characters to convert
     let mut range: u8 = 0;
+    // This vector will contain all the characters included by user for ease of access
     let mut validchars: Vec<u8> = Vec::new();
     if numop {
         range += NUMBERS.len() as u8;
