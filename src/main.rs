070130fa400e1b317b8db575ff5d3a069f1dccef
hk122001 <hlibk@proton.me>
2024-04-12 16:50:51 -0600

    Implement randomness through mouse movement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use rand_core::{RngCore, OsRng};
-use std::{str, process};
+use std::{str, process, thread, time};
+use mouse_position::mouse_position::Mouse;
 
 #[derive(Parser)]
 struct Args{
@@ -14,13 +15,15 @@ struct Args{
     uppercase: bool,
     #[arg(short, long, action)]
     special: bool,
+    #[arg(short, long, action)]
+    mouse: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
     if !args.numbers && !args.lowercase && !args.uppercase && !args.special {
-        eprintln!("Error: No character type was selected to generate. Please use --help and use the wanted flags to generate respective characters.");
+        eprintln!("Error: No character type was selected to generate. Use --help and select the wanted flags to generate respective characters.");
         process::exit(1);
     }
 
@@ -31,6 +34,13 @@ fn main() {
         Ok(b) => b,
         Err(_) => seconds_for_mouse = 10,
     };
+
+    if !args.mouse && seconds_for_mouse == 10{
+        eprintln!("Error: Failed to get randomness from system. In order to generate random values, mouse movement needs to be recorded. Use --mouse to record mouse movement.");
+        process::exit(1);
+    } else if args.mouse {
+        mouse_coords_to_random(&mut result, seconds_for_mouse, args.range)
+    }
 
     bytes_to_utfchars(&mut result, args.numbers, args.lowercase, args.uppercase, args.special);
 
@@ -43,6 +53,34 @@ fn main() {
     };
 
     println!("{}", result_str);
+}
+
+fn mouse_coords_to_random(result: &mut Vec<u8>, seconds: u8, vecrange: u8){
+    let iter: u16 = (seconds as u16) * 200;
+    let mut curriter: u16 = 0;
+    let mut lastx: i32 = 0;
+    let mut lasty: i32 = 0;
+    let mut i: u8 = 0;
+    while curriter <= iter {
+        let position = Mouse::get_mouse_position();
+        let (x, y) = match position {
+            Mouse::Position { x, y } => (x, y),
+            Mouse::Error => {
+                eprintln!("Error: Failed to get mouse position.");
+                process::exit(1);
+            },
+        };
+        if x == lastx && y == lasty {
+            continue;
+        }
+        lastx = x;
+        lasty = y;
+        let pos = x + y;
+        result[i as usize] = result[i as usize].wrapping_add((pos % 256) as u8);
+        i = (i + 1) % vecrange;
+        curriter += 1;
+        thread::sleep(time::Duration::from_millis(5));
+    }
 }
 
 fn bytes_to_utfchars(result: &mut Vec<u8>, numop: bool, lowop: bool, uppop: bool, specop: bool){
