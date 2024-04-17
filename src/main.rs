69a22dc4135446fdf436516bb97f4e2aa804cce4
hlibk <hlibk@proton.me>
2024-04-16 20:43:44 -0600

    Develop GUI and string generation functionality

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,14 @@
 // You should have received a copy of the GNU General Public License along with this
 // program. If not, see <https://www.gnu.org/licenses/>.
 
+#![windows_subsystem = "windows"]
+
 use rand_core::{RngCore, OsRng};
-use std::{str, process, thread, time};
+use std::{str, thread, time};
 use mouse_position::mouse_position::Mouse;
 use gtk::prelude::*;
 use gtk::{
-    gio, glib, Align, Application, ApplicationWindow, Button, Orientation, CheckButton, Entry, Label
+    glib, Align, Application, ApplicationWindow, Button, Orientation, CheckButton, Entry, Label, Window, AlertDialog, Spinner, PasswordEntry
 };
 
 const APP_ID: &str = "org.gtk_rs.hkstringgen";
@@ -28,44 +30,14 @@ fn main() -> glib::ExitCode{
     app.connect_activate(build_ui);
 
     app.run()
-    // let mut seconds_for_mouse: u8 = 10;
-
-    // // Gets randomness from system (supposed to be cryptographically safe)
-    // let mut result = vec![0u8; 8];
-    // match OsRng.try_fill_bytes(&mut result) {
-    //     Ok(b) => b,
-    //     Err(_) => seconds_for_mouse = 20,
-    // };
-
-    // // If failing to get randomness from system, force the user to use mouse coordinates
-    // // Also doubles the seconds required to generate randomness
-    // if true && seconds_for_mouse == 20{
-    //     eprintln!("Error: Failed to get randomness from system. In order to generate random values, mouse movement needs to be recorded. Use --mouse to record mouse movement.");
-    //     process::exit(1);
-    // } else if true {
-    //     mouse_coords_to_random(&mut result, seconds_for_mouse, 8)
-    // }
-
-    // bytes_to_utfchars(&mut result, true, true, true, true);
-
-    // // Converts utf8 bytes to readable characters
-    // let result_str = match str::from_utf8(&result) {
-    //     Ok(s) => s,
-    //     Err(e) => {
-    //         eprintln!("Error: {}.", e);
-    //         process::exit(1);
-    //     }
-    // };
-
-    // println!("{}", result_str);
 }
 
 fn build_ui(app: &Application) {
-    // button.connect_clicked(|button| {
-    //     button.set_label("Hello World!");
-    // });
-    let string_display = Entry::builder()
+    // Rust currently does not have an easy way of writing to clipboard, and most libraries
+    // are either OS specific or not working. For now, easiest way is to directly ^C and ^V
+    let string_display = PasswordEntry::builder()
         .editable(false)
+        .show_peek_icon(true)
         .build();
 
     let number_check = CheckButton::builder()
@@ -75,10 +47,11 @@ fn build_ui(app: &Application) {
     let range_entry = Entry::builder()
         .max_length(3)
         .max_width_chars(3)
+        .text("8")
         .build();
 
     let range_label = Label::builder()
-        .label("Number of characters to generate (from 0 to 255)")
+        .label("Number of characters to generate (from 1 to 255)")
         .build();
 
     let range_box = gtk::Box::builder()
@@ -101,8 +74,15 @@ fn build_ui(app: &Application) {
         .label("Include special characters")
         .build();
 
-    let button = Button::builder()
+    let mouse_check = CheckButton::builder()
+        .label("Include mouse movement for randomization")
+        .build();
+
+    let generate_button = Button::builder()
         .label("Generate random string")
+        .build();
+
+    let loading_spinner = Spinner::builder()
         .build();
 
     let gtk_box = gtk::Box::builder()
@@ -121,18 +101,70 @@ fn build_ui(app: &Application) {
     gtk_box.append(&lower_check);
     gtk_box.append(&upper_check);
     gtk_box.append(&spec_check);
-    gtk_box.append(&button);
+    gtk_box.append(&mouse_check);
+    gtk_box.append(&generate_button);
+    gtk_box.append(&loading_spinner);
 
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("My GTK App")
+        .title("hkstringgen")
         .child(&gtk_box)
         .build();
+
+    let alert = AlertDialog::builder()
+        .modal(true)
+        .build();
+
+    generate_button.connect_clicked(move |generate_button| {
+        let range: u8 = match range_entry.text().as_str().parse() {
+            Ok(v) => v,
+            Err(_) => 0,
+        };
+        if !number_check.is_active() && !lower_check.is_active() && !upper_check.is_active() && !spec_check.is_active() {
+            alert.set_detail("You need to specify a character type to generate");
+            alert.set_message("Error");
+            alert.show(None::<&Window>);
+        }else if range == 0 {
+            alert.set_detail("You need to specify a valid range");
+            alert.set_message("Error");
+            alert.show(None::<&Window>);
+        }else {
+            let mut seconds_for_mouse: u8 = 10;
+
+            // Gets randomness from system (supposed to be cryptographically safe)
+            let mut result = vec![0u8; range.into()];
+            // If failing to get randomness from system, force the user to use mouse coordinates
+            // Also doubles the seconds required to generate randomness
+            match OsRng.try_fill_bytes(&mut result) {
+                Ok(b) => b,
+                Err(_) => seconds_for_mouse = 20,
+            };
+            if mouse_check.is_active() || seconds_for_mouse == 20 {
+                mouse_coords_to_random(&mut result, seconds_for_mouse, 8, &alert);
+            }
+
+            bytes_to_utfchars(&mut result, number_check.is_active(), lower_check.is_active(), upper_check.is_active(), spec_check.is_active());
+
+            // Converts utf8 bytes to readable characters
+            let result_str = match str::from_utf8(&result) {
+                Ok(s) => s,
+                Err(_) => {
+                    alert.set_message("Failed to convert bytes to string");
+                    alert.set_detail("Error");
+                    alert.show(None::<&Window>);
+                    "0"
+                },
+            };
+            string_display.set_text(result_str);
+        }
+    });
+
+    
 
     window.present();
 }
 
-fn mouse_coords_to_random(result: &mut Vec<u8>, seconds: u8, vecrange: u8){
+fn mouse_coords_to_random(result: &mut Vec<u8>, seconds: u8, vecrange: u8, alert: &AlertDialog){
     let iter: u16 = (seconds as u16) * 200;
     let mut curriter: u16 = 0;
     let mut lastx: i32 = 0;
@@ -144,8 +176,10 @@ fn mouse_coords_to_random(result: &mut Vec<u8>, seconds: u8, vecrange: u8){
         let (x, y) = match position {
             Mouse::Position { x, y } => (x, y),
             Mouse::Error => {
-                eprintln!("Error: Failed to get mouse position.");
-                process::exit(1);
+                alert.set_message("Failed to get mouse position");
+                alert.set_detail("Error");
+                alert.show(None::<&Window>);
+                return;
             },
         };
 
